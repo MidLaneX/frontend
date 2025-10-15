2c67dd6a6d3a37fb6a47803fc8c3acabc013a23e
pasindu22 <pasindusathsara60@gmail.com>
2025-10-15 13:03:52 +0530

    build error fix

--- src/components/features/index.ts
+++ src/components/features/index.ts
@@ -6,7 +6,7 @@ export * from "./estimation";
 export * from "./scrum_board";
 export * from "./sprint";
 export * from "./backlog";
-export * from "./timeline";
+export * from "./timeLine";
 
 // Modal components
 export { default as CreateIssueModal } from "./CreateIssueModal";
